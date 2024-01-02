d848d9ccc0aa403e70f30feff0c3b0c8d27ea369
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2024-01-02 12:40:52 +0000

    chore(lxd > networkingl): allow declared lxd bridges

--- lxd/lxd-networking.nix
+++ lxd/lxd-networking.nix
@@ -2,6 +2,15 @@
 #
 # only forwarding
 # interfaces are managed in `lxd-preseed.yaml` by lxd
+{ config, lib, ... }:
+
+let
+  _LXD_PROFILES = {
+    "default" = (import ./profiles/default.nix);
+    "dev" = (import ./profiles/dev.nix);
+    "private0" = (import ./profiles/private0.nix);
+  };
+in
 {
   boot = {
     kernelModules = [ "nf_nat_ftp" ];
@@ -11,4 +20,11 @@
       "net.ipv4.conf.default.forwarding" = true;
     };
   };
+  
+  # allow static ipv4 for containers
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -i ${_LXD_PROFILES.default.network.name} -m comment --comment "lxd rule for ${_LXD_PROFILES.default.network.name}" -j ACCEPT
+    iptables -A INPUT -i ${_LXD_PROFILES.dev.network.name} -m comment --comment "lxd rule for ${_LXD_PROFILES.dev.network.name}" -j ACCEPT
+    iptables -A INPUT -i ${_LXD_PROFILES.private0.network.name} -m comment --comment "lxd rule for ${_LXD_PROFILES.private0.network.name}" -j ACCEPT
+  '';
 }
